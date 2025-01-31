abbd40a236d10df75c9636f5c16a311e46c2cde8
Jared Amaral <jaredamaral1229@gmail.com>
2025-01-31 13:29:14 -0800

    fix: refactored GPIO code

--- GPIO2.0/Core/Src/main.c
+++ GPIO2.0/Core/Src/main.c
@@ -1,28 +1,20 @@
 // Main.c
 
 #include "stm32f4xx.h"
+#include "gpio.h"
 
 int main(){
 
-    RCC->AHB1ENR |= (1 << 0); // Enable GPIOA clock
-
-    GPIOA->MODER |= (1 << 10); // Output Mode for Pin5 : (1 @ bit 2y)
-    GPIOA->MODER &= ~(1 << 11); // Output Mode for Pin5 : (0 @ bit 2y+1)
-
-    GPIOA->OTYPER &= ~(1 << 5); // Output Push-pull : (0 @ bit 5)
-
-    GPIOA->OSPEEDR |= (1 << 11); // Fast Speed : (1 @ bit 2y+1)
-    GPIOA->OSPEEDR &= ~(1 << 10); // Fast Speed : (0 @ bit 2y)
-
-    GPIOA->PUPDR &= ~((1 << 10) | (1 << 11)); // No Pull-up/down resistor : (00 @ bits 2y:2y+1)
+    GPIO_Init();
 
     while(1){
+        for (volatile int i = 0; i < 1000000; i++) {}  // Delay
+        // GPIOA->BSRR = (1 << 5); // Set Pin PA5 High
+        GPIO_Set_Pin();
 
         for (volatile int i = 0; i < 1000000; i++) {}  // Delay
-        GPIOA->BSRR = (1 << 5); // Set Pin PA5 High
-
-        for (volatile int i = 0; i < 1000000; i++) {}  // Delay
-        GPIOA->BSRR = (1 << 21); // Reset Pin PA5 Low
+        // GPIOA->BSRR = (1 << 21); // Reset Pin PA5 Low
+        GPIO_Reset_Pin();
     }
     return 0;
 }
