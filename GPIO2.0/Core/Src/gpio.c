7570ef8e76695b89f5b2e05879816a92b9199756
Jared Amaral <jaredamaral1229@gmail.com>
2025-02-02 12:18:46 -0800

    feat: defined PIN5

--- GPIO2.0/Core/Src/gpio.c
+++ GPIO2.0/Core/Src/gpio.c
@@ -5,19 +5,19 @@
 void GPIO_Init(){    
     RCC->AHB1ENR |= (1 << 0); // Enable GPIOA clock
 
-    GPIOA->MODER |= (1 << 10); // Output Mode for Pin5 : (1 @ bit 2y)
-    GPIOA->MODER &= ~(1 << 11); // Output Mode for Pin5 : (0 @ bit 2y+1)
+    GPIOA->MODER |= (1 << (GPIOA_PIN5 * 2)); // Output Mode for Pin5 : (1 @ bit 2y)
+    GPIOA->MODER &= ~(1 << (2 * GPIOA_PIN5 + 1)); // Output Mode for Pin5 : (0 @ bit 2y+1)
 
-    GPIOA->OTYPER &= ~(1 << 5); // Output Push-pull : (0 @ bit 5)
+    GPIOA->OTYPER &= ~(1 << GPIOA_PIN5); // Output Push-pull : (0 @ bit 5)
 
-    GPIOA->OSPEEDR |= (1 << 11); // Fast Speed : (1 @ bit 2y+1)
-    GPIOA->OSPEEDR &= ~(1 << 10); // Fast Speed : (0 @ bit 2y)
+    GPIOA->OSPEEDR |= (1 << (2 * GPIOA_PIN5 + 1)); // Fast Speed : (1 @ bit 2y+1)
+    GPIOA->OSPEEDR &= ~(1 << (2 * GPIOA_PIN5)); // Fast Speed : (0 @ bit 2y)
 
-    GPIOA->PUPDR &= ~((1 << 10) | (1 << 11)); // No Pull-up/down resistor : (00 @ bits 2y:2y+1)
+    GPIOA->PUPDR &= ~((1 << (2 * GPIOA_PIN5)) | (1 << (2 * GPIOA_PIN5 + 1))); // No Pull-up/down resistor : (00 @ bits 2y:2y+1)
 }
 
 void GPIO_Set_Pin(){
-    GPIOA->BSRR = (1 << 5); // Set Pin PA5 High
+    GPIOA->BSRR = (1 << GPIOA_PIN5); // Set Pin PA5 High
 }
 
 void GPIO_Reset_Pin(){
